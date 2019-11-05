5f29adf537f1c469f94a9cde9f33cbc5e194dc5b
Goodness Ezeokafor <gootech442@yahoo.com>
2019-11-05 02:25:17 +0100

    Revert "Revert "mistake""
    This reverts commit f054912e0f554438b317305293260115e4087687.

--- image_reduction/settings/base.py
+++ image_reduction/settings/base.py
@@ -141,3 +141,4 @@ MESSAGE_TAGS = {
 STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new!
